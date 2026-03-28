dc1f58da2ca895c6e29f99aa9eebba7612b34ec4
John DeWorken <jdeworken@advantage.com>
2026-03-27 17:54:00 -0700

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,3 +1,7 @@
 import Stripe from "stripe";
+
 const key = process.env.STRIPE_SECRET_KEY;
-export const stripe = key ? new Stripe(key, { apiVersion: "2024-06-20" }) : null;
+
+export const stripe = key
+  ? new Stripe(key, { apiVersion: "2023-10-16" })
+  : null;
